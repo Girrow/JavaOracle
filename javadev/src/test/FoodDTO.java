974d85625301d60be625a5f8310e513ae9a6bee6
harin <teri941227@gmail.com>
2021-03-24 11:39:23 +0900

    modify

--- javadev/src/test/FoodDTO.java
+++ javadev/src/test/FoodDTO.java
@@ -1,8 +1,10 @@
 package test;
 
 public class FoodDTO {
+	OrientalDTO a = new OrientalDTO();
+	
 	public static void main(String[] args) {
-		OrientalDTO a = new OrientalDTO();
-		System.out.println(a);
+		FoodDTO fd = new FoodDTO();
+		System.out.println(fd.a.getId());
 	}
 }
