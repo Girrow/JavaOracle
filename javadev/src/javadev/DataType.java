1e7688c72ea230f2debc1004a832459cd078c541
harin <teri941227@gmail.com>
2021-03-24 12:18:28 +0900

    1일차 점심

---
+++ javadev/src/javadev/DataType.java
@@ -0,0 +1,10 @@
+package javadev;
+
+public class DataType {
+	public static void main(String[] args) {
+		boolean b1 = false;
+		System.out.printf("b1 : %s\n", b1);
+		char c = 65;
+		System.out.println("c : "+c);
+	}
+}
---
+++ javadev/src/javadev/OpTest01.java
@@ -0,0 +1,17 @@
+package javadev;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class OpTest01 {
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			System.out.print("정수 A값 입력 : ");
+			String a = br.readLine();
+			StringTokenizer st = new StringTokenizer(a);
+			while(st.hasMoreTokens()) System.out.println(Integer.parseInt(st.nextToken()));
+		} catch (Exception e) {}
+	}
+}
